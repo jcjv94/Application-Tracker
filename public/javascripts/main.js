9cfed7a8505d4453899c7ffc46df3dab5d5484d3
Jon Carlo Vargas <jcjv94@gmail.com>
2019-12-02 16:32:25 -0800

    materializing

---
+++ public/javascripts/main.js
@@ -0,0 +1,10 @@
+console.log('hello')
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.datepicker');
+//     var instances = M.Datepicker.init(elems, options);
+//   });
+
+$(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
--- server.js
+++ server.js
@@ -34,7 +34,8 @@ app.use(session({
 app.use(passport.initialize());
  app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use('/', indexRouter);
 // app.use('/', usersRouter);
