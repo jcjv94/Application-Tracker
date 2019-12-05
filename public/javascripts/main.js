19d733e9bc6769636960aab27f6906cce17c4afa
Jon Carlo Vargas <jcjv94@gmail.com>
2019-12-05 11:08:04 -0800

    screenshots for readme

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,7 @@
-// console.log('hello')
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.datepicker');
-//     var instances = M.Datepicker.init(elems, options);
-//   });
 
 $(document).ready(function(){
     $('.datepicker').datepicker();
     $('.timepicker').timepicker();
     $('.collapsible').collapsible();
   });
-
-  // $(document).ready(function(){
-  //   $('.timepicker').timepicker();
-  // });
-      
---
+++ readme.md
@@ -0,0 +1,8 @@
+# JobApp Tracker
+
+[Click to View] (https://jobapp-trkr.herokuapp.com/)
+
+### Purpose:
+
+Have you ever applied to a bunch of jobs and then forgotten what you've applied to? The purpose of this app is to help you track those job applications with ease. This app lets you add job applications, store notes on them, and update as needed.
+
